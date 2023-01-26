99cef87d71f5404cbb5eb443a4e06b77d098bb42
Tyfaspo <svsh991@gmail.com>
2023-01-26 11:27:46 +0600

    lab2

--- hello.py
+++ hello.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+a = int(input('Enter A:\n'))
+b = int(input('Enter B:\n'))
+
+def add(a, b):
+    sum = a + b
+    print(f'Summ of `a` and `b` = {sum}')
+
+def subs(a, b):
+    result = a - b
+    print(f'Resulut of substraction of `a` and `b` = {result}')
+
+def mult(a, b):
+    result = a * b
+    print(f'Result of multiplication of `a` and `b` = {result}')
+
+def div(a, b):
+    result = a + b
+    print(f'Result of division of `a` and `b` = {result}')
+
+add(a,b)
+subs(a,b)
+mult(a,b)
+div(a,b)
